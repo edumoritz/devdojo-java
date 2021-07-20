95ea9e776180852f7f3864b6e46f8a2e06b54561
edumoritz <eduardo.moritz@hotmail.com>
2021-07-20 20:27:12 -0300

    associacoes

---
+++ src/academy/devdojo/maratonajava/javacore/Gassociacao/test/JogadorTest.java
@@ -0,0 +1,51 @@
+package academy.devdojo.maratonajava.javacore.Gassociacao.test;
+
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Jogador;
+import academy.devdojo.maratonajava.javacore.Gassociacao.dominio.Time;
+
+public class JogadorTest {
+    public static void main(String[] args) {
+        Test01();
+        Test02();
+    }
+
+    private static void Test01() {
+        Jogador jogador1 = new Jogador("Pelé");
+        Jogador jogador2 = new Jogador("Romário");
+        Jogador jogador3 = new Jogador("Cafu");
+        Jogador[] jogadores = {jogador1, jogador2, jogador3};
+
+        for (Jogador jogador : jogadores) {
+            jogador.imprime();
+        }
+    }
+
+    private static void Test02() {
+        Jogador jogador1 = new Jogador("Pelé");
+        Time time = new Time("Seleção Brasileira");
+
+        jogador1.setTime(time);
+
+        jogador1.imprime();
+    }
+
+    private static void Test03() {
+        Jogador jogador = new Jogador("Cafu");
+        Jogador jogador2 = new Jogador("Pelé");
+        Time time = new Time("Brasil");
+        Jogador[] jogadores = {jogador, jogador2};
+
+        jogador.setTime(time);
+        jogador2.setTime(time);
+
+        time.setJogadores(jogadores);
+
+        System.out.println("--- Jogador ---");
+
+        jogador.imprime();
+
+        System.out.println("--- Time ---");
+
+        time.imprime();
+    }
+}
