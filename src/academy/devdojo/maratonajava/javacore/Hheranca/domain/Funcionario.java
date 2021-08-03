3240da0b0a1ffe5c2a0862d4148ff8a1fb568baa
edumoritz <eduardo.moritz@hotmail.com>
2021-08-03 20:56:37 -0300

    heranca super

--- src/academy/devdojo/maratonajava/javacore/Hheranca/domain/Funcionario.java
+++ src/academy/devdojo/maratonajava/javacore/Hheranca/domain/Funcionario.java
@@ -1,7 +1,13 @@
 package academy.devdojo.maratonajava.javacore.Hheranca.domain;
 
 public class Funcionario extends Pessoa {
+
     private double salario;
+
+    public void imprime(){
+        super.imprime();
+        System.out.println(this.salario);
+    }
 
     public double getSalario() {
         return salario;
