e4e93174ddab4f3c1c0c1c84020c79eb992507de
edumoritz <eduardo.moritz@hotmail.com>
2021-09-04 17:39:32 -0300

    enum 02

--- src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest.java
+++ src/academy/devdojo/maratonajava/javacore/Kenum/test/ClienteTest.java
@@ -2,17 +2,14 @@ package academy.devdojo.maratonajava.javacore.Kenum.test;
 
 import academy.devdojo.maratonajava.javacore.Kenum.dominio.Cliente;
 import academy.devdojo.maratonajava.javacore.Kenum.dominio.TipoCliente;
+import academy.devdojo.maratonajava.javacore.Kenum.dominio.Cliente.TipoPagamento;
 
 public class ClienteTest {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("Tsubasa",TipoCliente.PESSOA_FISICA);
-        Cliente cliente3 = new Cliente("Tsubasa",TipoCliente.PESSOA_FISICA);
-        Cliente cliente4 = new Cliente("Tsubasa",TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente1 = new Cliente("Tsubasa", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Akira",TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
 
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
-        System.out.println(cliente4);
     }
 }
