c685995aeea7a6af7b06528b1da2fbbcaffbbb26
edumoritz <eduardo.moritz@hotmail.com>
2021-09-23 13:32:34 -0300

    interface 04

--- src/academy/devdojo/maratonajava/javacore/Minterfaces/domain/DatabaseLoader.java
+++ src/academy/devdojo/maratonajava/javacore/Minterfaces/domain/DatabaseLoader.java
@@ -15,4 +15,8 @@ public class DatabaseLoader implements DataLoader, DataRemover {
     public void remove() {
         System.out.println("Removendo dados do banco de dados");
     }
+
+    public static void retrieveMaxDataSize(){
+        System.out.println("Dentro do retrieveMaxDataSize na classe DatabaseLoader");
+    }
 }
