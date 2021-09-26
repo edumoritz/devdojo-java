84d8b86abe4035ff68f86e6f1d556c7ec7b78a88
edumoritz <eduardo.moritz@hotmail.com>
2021-09-26 10:12:56 -0300

    polimorfismo pt 01-02

---
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/ProdutoTest.java
@@ -0,0 +1,35 @@
+package academy.devdojo.maratonajava.javacore.Npolimorfismo.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Produto;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.CalculadoraImposto;
+
+public class ProdutoTest {
+    public static void main(String[] args) {
+//        Aula01();
+        Aula02();
+    }
+
+    private static void Aula01() {
+        Computador computador = new Computador("NUC10i7", 11000);
+        Tomate tomate = new Tomate("Tomate Siciliano", 10);
+        CalculadoraImposto.calcularImpostoComputador(computador);
+        System.out.println("-------------------------");
+        CalculadoraImposto.calcularImpostoTomate(tomate);
+    }
+
+    private static void Aula02() {
+        Produto produto = new Computador("Ryzen 9", 3000);
+        System.out.println(produto.getNome());
+        System.out.println(produto.getValor());
+        System.out.println(produto.calcularImposto());
+        System.out.println("------------");
+
+        Produto produto2 = new Tomate("Americano", 20);
+        System.out.println(produto2.getNome());
+        System.out.println(produto2.getValor());
+        System.out.println(produto2.calcularImposto());
+    }
+}
