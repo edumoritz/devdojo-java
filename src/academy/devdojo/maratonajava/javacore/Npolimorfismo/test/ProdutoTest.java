1c188beb7a624e6997b7fdb412f085359262b292
edumoritz <eduardo.moritz@hotmail.com>
2021-09-29 17:03:54 -0300

    polimorfismo 03

--- src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/ProdutoTest.java
+++ src/academy/devdojo/maratonajava/javacore/Npolimorfismo/test/ProdutoTest.java
@@ -3,21 +3,26 @@ package academy.devdojo.maratonajava.javacore.Npolimorfismo.test;
 import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Computador;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Produto;
+import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Televisao;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.dominio.Tomate;
 import academy.devdojo.maratonajava.javacore.Npolimorfismo.servico.CalculadoraImposto;
 
 public class ProdutoTest {
     public static void main(String[] args) {
-//        Aula01();
-        Aula02();
+        Aula01();
+//        Aula02();
     }
 
     private static void Aula01() {
         Computador computador = new Computador("NUC10i7", 11000);
         Tomate tomate = new Tomate("Tomate Siciliano", 10);
-        CalculadoraImposto.calcularImpostoComputador(computador);
+        Televisao tv = new Televisao("Samsung 50\" ", 5000);
+
+        CalculadoraImposto.calcularImposto(computador);
         System.out.println("-------------------------");
-        CalculadoraImposto.calcularImpostoTomate(tomate);
+        CalculadoraImposto.calcularImposto(tomate);
+        System.out.println("-------------------------");
+        CalculadoraImposto.calcularImposto(tv);
     }
 
     private static void Aula02() {
