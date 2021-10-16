445e025e81f68f47a28e091717cf4eaeddd42144
edumoritz <eduardo.moritz@hotmail.com>
2021-10-16 14:14:12 -0300

    excecoes 5-6-7-8-9

---
+++ src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/TryWithResourcesTest01.java
@@ -0,0 +1,38 @@
+package academy.devdojo.maratonajava.javacore.Oexception.exception.test;
+
+import academy.devdojo.maratonajava.javacore.Oexception.exception.dominio.Leitor01;
+import academy.devdojo.maratonajava.javacore.Oexception.exception.dominio.Leitor02;
+
+import java.io.*;
+
+public class TryWithResourcesTest01 {
+    public static void main(String[] args) {
+        lerArquivo();
+    }
+
+    public static void lerArquivo() {
+        try (Leitor01 leitor1 = new Leitor01();
+             Leitor02 leitor2 = new Leitor02()) {
+
+        } catch (IOException e) {
+
+        }
+    }
+
+    public static void lerArquivo2() {
+        Reader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("teste.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException exception) {
+                exception.printStackTrace();
+            }
+        }
+    }
+}
