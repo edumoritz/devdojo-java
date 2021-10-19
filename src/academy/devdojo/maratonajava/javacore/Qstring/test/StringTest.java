3b04a2b79c8c4bf940da75015d6049f978369d35
edumoritz <eduardo.moritz@hotmail.com>
2021-10-19 20:18:15 -0300

    wrapper

---
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.Qstring.test;
+
+public class StringTest {
+    public static void main(String[] args) {
+        Test01();
+    }
+
+    private static void Test01() {
+        String nome = "William"; //String constant pool
+        String nome2 = "William";
+        nome = nome.concat(" Suane");
+        System.out.println(nome);
+        System.out.println(nome == nome2);
+        String nome3 = new String("William"); // 1 variavel de referência, 2 objeto do tipo string, 3 uma string no pool de string
+        System.out.println(nome2 == nome3);
+        System.out.println(nome2 == nome3.intern());
+    }
+}
