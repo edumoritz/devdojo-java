536a14312a34b7ee6bf33659ed452f3d367f4f90
edumoritz <eduardo.moritz@hotmail.com>
2021-10-20 10:41:41 -0300

    performance de string

--- src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest.java
+++ src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest.java
@@ -3,6 +3,7 @@ package academy.devdojo.maratonajava.javacore.Qstring.test;
 public class StringTest {
     public static void main(String[] args) {
         Test01();
+        Test02();
     }
 
     private static void Test01() {
@@ -15,4 +16,20 @@ public class StringTest {
         System.out.println(nome2 == nome3);
         System.out.println(nome2 == nome3.intern());
     }
+
+    private static void Test02() {
+        String nome = "Luffy";
+        String numeros = "012345";
+        System.out.println(nome.charAt(0));
+        System.out.println(nome.length());
+        System.out.println(nome.replace("f","l"));
+        System.out.println(nome.toLowerCase());
+        System.out.println(nome.toUpperCase());
+        System.out.println(numeros.length());
+        System.out.println(numeros.substring(3,numeros.length()));
+        System.out.println(nome.trim());
+    }
+
+
+
 }
